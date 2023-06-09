010b768f576bdb8ef1fbcaa835b0186c2a2a4bd3
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-09 12:16:16 +0300

    Create star.py

---
+++ star.py
@@ -0,0 +1,12 @@
+import turtle
+
+board = turtle.Screen()
+boardTitle = turtle.title("Star")
+boardBgColor = turtle.bgcolor("red")
+
+turtle1 = turtle.Turtle()
+turtle1.pencolor("white")
+turtle1.pensize(5)
+
+
+turtle.done()
